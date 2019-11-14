76d58573337013abe1465e4f23cb106bec5b1b61
Nineodes19 <3100863513@qq.com>
2019-11-14 23:40:56 +0800

    提交代码

---
+++ test_11.14/test_11.14/test.c
@@ -0,0 +1,66 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	char* pstr = "hello world!";
+//	printf("%s\n", pstr);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	char str1[] = "hello.";
+//	char str2[] = "hello.";
+//	char *str3 = "hello.";
+//	char *str4 = "hello.";
+//
+//
+//	//str1 and str2 are not same
+//	
+//	if (str1 == str2)
+//	{
+//		printf("str1 and str2 are same\n");
+//	}
+//	else
+//	{
+//		printf("str1 and str2 are not same\n");
+//	}
+//
+//	//输出：str3 and str4 are same
+//	//str3 和str4指向同一个字符串，实际上是指向同一块内存
+//	if (str3 == str4)
+//	{
+//		printf("str3 and str4 are same\n");
+//	}
+//	else
+//	{
+//		printf("str3 and str4 are not same\n");
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	////数组指针（实际上是指针）
+//	//int *arr1[10];//整形指针的数组
+//	//char *arr2[4];//字符指针的数组
+//	//char **arr3[5];//二级字符指针的数组
+//
+//	//
+//	int *p1[10];//指针数组
+//	int(*p2)[10];//数组指针，
+//	return 0;
+//}
+
+#include<stdio.h>
+
+int main()
+{
+
+	int arr[10] = { 0 };
+	return 0;
+}
