fd0bdc3733b1a9bddac95f797feef06336cc2567
Nineodes19 <3100863513@qq.com>
2019-12-22 20:57:59 +0800

    提交代码

--- test_12.22/src/code1/MathClass.java
+++ test_12.22/src/code1/MathClass.java
@@ -11,11 +11,14 @@ import java.util.Random;
 public class MathClass {
     public static void main(String[] args){
 
-        Random r = new Random();
-        for (int i = 0; i < 10; i++) {
-            int num = r.nextInt(1000);
-            System.out.println(num);
-        }
+        int a = 10;
+        Integer b = 29;
+        System.out.println(b.byteValue());
+//        Random r = new Random();
+//        for (int i = 0; i < 10; i++) {
+//            int num = r.nextInt(1000);
+//            System.out.println(num);
+//        }
 
 //        double a = 12.86;
 //        int b = (int)a;
---
+++ test_code/src/nowcoder/Solution1001.java
@@ -0,0 +1,36 @@
+package nowcoder;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @program:test_code
+ * @author: Nine_odes
+ * @description:明明的随机数
+ * @create:2019-12-22 13:52
+ */
+public class Solution1001 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int N = sc.nextInt();
+            int[]array = new int[N];
+
+            for(int i = 0; i < N; i++){
+                //array[i] = (int)(Math.random() * 999 + 1);
+                array[i] = sc.nextInt();
+            }
+            Arrays.sort(array);
+            System.out.println(array[0]);
+            for (int i = 1; i < N; i++) {
+                if(array[i] != array[i-1]){
+                    System.out.println(array[i]);
+                }
+            }
+        }
+
+    }
+}
