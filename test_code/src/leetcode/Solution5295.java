738cfa8bd6f9a7e7ce634b8ea78b34904c3e78d0
Nineodes19 <3100863513@qq.com>
2019-12-29 22:44:26 +0800

    提交代码

---
+++ test_code/src/leetcode/Solution5295.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @program:test_code
+ * @author: Nine_odes
+ * @description:和为零的N个唯一整数
+ * @create:2019-12-29 22:10
+ */
+public class Solution5295 {
+    public int[] sumZero(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n/2; i++) {
+            arr[i*2] = i+1;
+            arr[i*2+1] = -(i+1);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Solution5295 s = new Solution5295();
+        Arrays.toString(s.sumZero(5));
+    }
+}
