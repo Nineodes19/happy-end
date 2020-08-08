fbe65f876ee94a32dd27b882a56e876484669028
Nineodes19 <3100863513@qq.com>
2020-08-08 23:16:15 +0800

    提交代码

---
+++ test_8.8/src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+
+
+
+public class Controller {
+    @FXML
+    public void buttonHasClicked(Event event){
+        System.out.println("hello world");
+    }
+}
