103806579e2dbeebcef15a19ece0f71bc8dd894d
Nineodes19 <3100863513@qq.com>
2020-10-01 23:25:30 +0800

    提交代码

---
+++ test_10.1/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+
+/**
+ * @program:test_10.1
+ * @author:Nine_odes
+ * @description:
+ * @create:2020-10-01 23:23
+ **/
+public class Solution {
+    public int[] smallestK(int[] arr,int k){
+        if(null == arr || k <= 0){
+            return new int[0];
+        }
+        PriorityQueue<Integer> q = new PriorityQueue<>(arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            q.offer(arr[i]);
+        }
+        int[]ret = new int[k];
+        for (int i = 0; i < k; i++) {
+            ret[i] = q.poll();
+        }
+
+        return ret;
+    }
+}
