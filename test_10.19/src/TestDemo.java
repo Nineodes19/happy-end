ca9f50bb7b902579532dff5dc5891dc00810fe38
Nineodes19 <3100863513@qq.com>
2020-10-19 23:35:30 +0800

    提交代码

---
+++ test_10.19/src/TestDemo.java
@@ -0,0 +1,13 @@
+/**
+ * @program:test_10.19
+ * @author:Nine_odes
+ * @description:
+ * @create:2020-10-19 23:34
+ **/
+public class TestDemo {
+    public static void main(String[] args) {
+        int i = 99;
+        int j = 1;
+        System.out.println(i+++j);
+    }
+}
